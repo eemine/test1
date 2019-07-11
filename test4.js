bb733a6f507a9a5e0856f9a8ff2abe774602d1a6
Amina Ragimli <amina@Aminas-MacBook-Air.local>
2019-07-11 22:24:37 +0300

    test4

---
+++ test4.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function(req, res) {
+  res.send("Hello World");
+});
+
+const users = [1, 2, 3, 4];
+const addUser = (name, surname, age) =>
+  users.push({
+    name,
+    surname,
+    age
+  });
+const getAllUsers = () => users;
+
+app.get("/users", function(req, res) {
+  const allUsers = getAllUsers();
+  res.send(allUsers);
+});
+
+app.listen(3000);
